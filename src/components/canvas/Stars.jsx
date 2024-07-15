ac21d0bf217da4bb59741cd27307c0374dcb0fe0
hang nhung <hangbichnhung@gmail.com>
2024-07-15 21:15:22 +0700

    fix - computed radius is NaN

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -6,11 +6,11 @@ import React, { Suspense, useRef } from "react";
 const Stars = (props) => {
   const ref = useRef();
 
-  const sphere = random.inSphere(new Float32Array(5000), { radius: 1.2 });
+  const sphere = random.inSphere(new Float32Array(6000), { radius: 1.2 });
 
   useFrame((state, delta) => {
     ref.current.rotation.x -= delta / 10;
-    ref.current.rotation.y -= delta / 10;
+    ref.current.rotation.y -= delta / 15;
   });
 
   return (
